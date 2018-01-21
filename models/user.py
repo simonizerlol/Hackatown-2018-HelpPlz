47eaba0c8ef5cd0d4c4e73e3eb102925ef6010bd
Jaleel Jolly <jaleeljj@live.com>
2018-01-20 23:03:04 -0500

    Update user.py

--- models/user.py
+++ models/user.py
@@ -3,3 +3,10 @@ class User():
         self.email = email
         self.username = username
         self.password = password
+        
+    def json(self):
+        return {
+    'email': self.email,
+    'username': self.username,
+    'password': self.password
+    }
